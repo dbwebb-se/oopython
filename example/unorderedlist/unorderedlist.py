72261d621bf1167c98dd80d4a815b0aa39bb1ed6
Kenneth Lewenhagen <lewenhagen@live.com>
2016-05-19 11:17:19 +0200

    fixed unorderedlist.py

--- example/unorderedlist/unorderedlist.py
+++ example/unorderedlist/unorderedlist.py
@@ -70,17 +70,4 @@ class UnorderedList:
         """
         Removes item from list
         """
-        current = self.head
-        previous = None
-        found = False
-        while not found:
-            if current.get_data() == item:
-                found = True
-            else:
-                previous = current
-                current = current.get_next()
-
-        if previous is None:
-            self.head = current.get_next()
-        else:
-            previous.set_next(current.get_next())
+        # insert method
