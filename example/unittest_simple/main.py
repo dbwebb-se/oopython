0b06bc6c51d0fd8e0bddf7177366f8cbbef69d63
Kenneth Lewenhagen <lewenhagen@live.com>
2017-12-12 10:53:25 +0100

    renamed unittest folders

---
+++ example/unittest_simple/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Test pydoc
+"""
+
+def return_name(name):
+    """Returns string"""
+
+    return "Hello " + str(name)
+
+def add_numbers(num_one, num_two):
+    """Adds the numbers num_one and num_two"""
+    return num_one + num_two
