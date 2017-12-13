f172c5ae4a2ca5519eaf9c4d82a02c63353d4a30
Kenneth Lewenhagen <lewenhagen@live.com>
2017-12-13 09:58:09 +0100

    added unittest example

---
+++ example/unittest/phone.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+"""
+Main object file
+"""
+
+class Phone:
+    """
+    Phone class
+    """
+    def __init__(self, manufacturer, model, os):
+        """
+        init method
+        """
+        self.manufacturer = manufacturer
+        self.model = model
+        self.os = os
+        self.owner = "No owner yet"
+        self.phonebook = []
+
+    def get_manufacturer(self):
+        """ Returns the manufacturer """
+        return self.manufacturer
+
+    def get_model(self):
+        """ Returns the model """
+        return self.model
+
+    def get_os(self):
+        """ Returns the os """
+        return self.os
+
+    def get_owner(self):
+        """ Returns the owner """
+        return self.owner
+
+    def change_owner(self, new_owner):
+        """ Changes the owner of the phone """
+        self.owner = new_owner
+
+    def has_contacts(self):
+        """ Returns True if phonebook has contacts, else False """
+        return True if self.phonebook else False
+
+    def get_contacts_length(self):
+        """ Returns amount of contacts """
+        return len(self.phonebook)
+
+    def add_contact(self, name, number):
+        """ Add contact to phonebook """
+        self.phonebook.append((name, number))
+
+    def get_contact(self, name):
+        """ Returns tuple with name and number """
+        for person in self.phonebook:
+            if person[0] == name:
+                return person
---
+++ example/unittest/test.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+Unittest file for Phone
+"""
+
+import unittest
+from phone import Phone
+
+class Testcase(unittest.TestCase):
+    """Class Testcase"""
+    phone = Phone("Samsung", "Galaxy S8", "Android")
+    phone_two = Phone("Apple", "iPhone 8", "iOS")
+
+    # Tests if the objects are the same
+    def test_A_equal_objects(self):
+        """ Should return True, they are not the same """
+        self.assertIsNot(self.phone, self.phone_two)
+
+    # Tests if the objects are instances of Person
+    def test_B_are_object_instance_of(self):
+        """ Should return True, is is instance of Phone """
+        self.assertIsInstance(self.phone, Phone)
+
+    # Tests if owner returns correct when none is set
+    def test_C_no_owner(self):
+        """Should return 'No owner yet' if correct"""
+        self.assertEqual(self.phone.get_owner(), "No owner yet")
+
+    # Tests if owner returns corect when owner is set
+    def test_D_set_owner(self):
+        """Should return 'Pelle' if correct"""
+        self.phone.change_owner("Pelle")
+        self.assertEqual(self.phone.get_owner(), "Pelle")
+
+    # Tests property: manufacturer
+    def test_E_prop_manufacturer(self):
+        """Should return 'Samsung' if correct"""
+        self.assertEqual(self.phone.get_manufacturer(), "Samsung")
+
+    # Tests property: model
+    def test_F_prop_model(self):
+        """Should return 'S8' if correct"""
+        self.assertEqual(self.phone.get_model(), "Galaxy S8")
+
+    # Tests property: os
+    def test_G_prop_os(self):
+        """Should return 'Android' if correct"""
+        self.assertEqual(self.phone.get_os(), "Android")
+
+    # Test if phonebook is empty
+    def test_H_empty_phonebook(self):
+        """Should return False if phonebook is empty"""
+        self.assertFalse(self.phone.has_contacts())
+
+    # Test if phonebook is not empty
+    def test_I_not_empty_phonebook(self):
+        """Should return True if phonebook has contacts"""
+        self.phone.add_contact("Andreas", 12345)
+        self.phone.add_contact("Emil", 67890)
+        self.assertTrue(self.phone.has_contacts())
+
+    # Test phonebook length
+    def test_J_phonebook_length(self):
+        """Should return the number 2"""
+        self.assertEqual(self.phone.get_contacts_length(), 2)
+
+    # Test get contact that not exists
+    def test_K_faulty_contact(self):
+        """Should return None"""
+        self.assertIsNone(self.phone.get_contact("Kenneth"))
+
+    # Test get contact that exists
+    def test_L_get_contact(self):
+        """Should return tuple with contact name and number"""
+        self.assertEqual(self.phone.get_contact("Andreas"), ("Andreas", 12345))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
