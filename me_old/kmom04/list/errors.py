ed9e6da4b8243035a885e1c41c0741d07515cae3
Andreas Arnesson <Andreas_osby@hotmail.com>
2019-02-27 07:52:02 +0100

    removed old folder från repo

--- me_old/kmom04/list/errors.py
+++
@@ -1,3 +0,0 @@
-class ValueTooBigError(Exception):
-    """Custom exception for value to big"""
-    pass
--- me_old/kmom04/list/node.py
+++
@@ -1,5 +0,0 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.previous = None
