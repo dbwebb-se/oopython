819edec628f0e1d7b2d4c539441f3d827a16815b
Martin Borg <martin.d@live.se>
2021-02-02 16:03:58 +0100

    added queue example

---
+++ example/queue/main.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+""" Main file with Handler class """
+
+import sys
+import inspect
+from queue import Queue
+from errors import EmptyQueueException
+
+
+class Handler:
+    """ Handler class """
+
+    _OPTIONS = {
+        "1": "add_to_queue",
+        "2": "remove_from_queue",
+        "3": "get_first_item",
+        "4": "get_queue_size",
+        "5": "is_queue_empty",
+        "q": "quit"
+    }
+
+    def __init__(self):
+        """ Initialize class """
+        self.queue = Queue()
+        self.start()
+
+
+    def _get_method(self, method_name):
+        """
+        Uses function getattr() to dynamically get value of an attribute.
+        """
+        return getattr(self, self._OPTIONS[method_name])
+
+
+    def _print_menu(self):
+        """
+        Use docstring from methods to print options for the program.
+        """
+        menu = ""
+
+        for key in sorted(self._OPTIONS):
+            method = self._get_method(key)
+            docstring = inspect.getdoc(method)
+
+            menu += "{choice}: {explanation}\n".format(
+                choice=key,
+                explanation=docstring
+            )
+
+        print(chr(27) + "[2J" + chr(27) + "[;H")
+        print(menu)
+
+    def add_to_queue(self):
+        """ Adds a node to the queue. """
+        value = input("\nAdd a value: \n>>> ")
+        self.queue.enqueue(value)
+        print(f"{value} has been added.")
+
+    def remove_from_queue(self):
+        """ Removes the first node from the queue. """
+        try:
+            print(f"{self.queue.dequeue()} has been removed.")
+        except EmptyQueueException as e:
+            print(f"Error: {e}")
+
+    def get_first_item(self):
+        """ Prints the value of the first queue node. """
+        try:
+            print(self.queue.peek())
+        except EmptyQueueException as e:
+            print(f"Error: {e}")
+
+    def get_queue_size(self):
+        """ Shows the queue length. """
+        print(self.queue.size())
+
+    def is_queue_empty(self):
+        """ Shows if the queue is empty or not. """
+        print(self.queue.is_empty())
+
+    @staticmethod
+    def quit():
+        """ Quit the program """
+        sys.exit()
+
+    def start(self):
+        """ Start method """
+        while True:
+            self._print_menu()
+            choice = input("Enter menu selection:\n-> ")
+
+            try:
+                self._get_method(choice.lower())()
+            except KeyError:
+                print("Invalid choice!")
+
+            input("\nPress any key to continue ...")
+
+if __name__ == "__main__":
+    Handler()
---
+++ example/queue/tests.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+""" Module for unittests """
+
+import unittest
+from queue import Queue
+from node import Node
+from errors import EmptyQueueException
+
+
+class TestQueue(unittest.TestCase):
+    """ Submodule for unittests, derives from unittest.TestCase """
+
+    first_node_value = "value"
+    second_node_value = "value_two"
+
+    def setUp(self):
+        """ Create object for all tests """
+        # Arrange
+        self.queue = Queue()
+
+    def tearDown(self):
+        """ Remove dependencies after test """
+        self.queue = None
+
+    def test_enqueue(self):
+        """
+        Test that one can add a node to the queue.
+        """
+        self.queue.enqueue(TestQueue.first_node_value)
+        self.queue.enqueue(TestQueue.second_node_value)
+        self.assertIsInstance(self.queue.head, Node)
+        self.assertEqual(self.queue.head.data, TestQueue.first_node_value)
+
+    def test_peek(self):
+        """
+        Test if the peek method returns the first node's data.
+        """
+        self.queue.enqueue(TestQueue.first_node_value)
+        self.queue.enqueue(TestQueue.second_node_value)
+        self.assertEqual(self.queue.peek(), TestQueue.first_node_value)
+
+    def test_is_empty(self):
+        """
+        Tests if the is_empty method returns the correct boolean.
+        """
+        self.assertEqual(self.queue.is_empty(), True)
+        self.queue.enqueue(TestQueue.first_node_value)
+        self.assertEqual(self.queue.is_empty(), False)
+
+    def test_size(self):
+        """
+        Tests if the size method should return the correct size.
+        """
+        self.assertEqual(self.queue.size(), 0)
+        self.queue.enqueue(TestQueue.first_node_value)
+        self.queue.enqueue(TestQueue.second_node_value)
+        self.assertEqual(self.queue.size(), 2)
+
+    def test_dequeue(self):
+        """
+        Tests if dequeue removes the node and returns its data.
+        """
+        self.queue.enqueue(TestQueue.first_node_value)
+        self.queue.enqueue(TestQueue.second_node_value)
+        self.assertEqual(self.queue.dequeue(), TestQueue.first_node_value)
+        self.assertEqual(self.queue.peek(), TestQueue.second_node_value)
+        self.assertEqual(self.queue.size(), 1)
+
+    def test_dequeue_empty(self):
+        """
+        An EmptyQueueException should be raised when one tries to remove a node
+        from an empty queue.
+        """
+        with self.assertRaises(EmptyQueueException) as _:
+            self.queue.dequeue()
+
+    def test_peek_empty(self):
+        """
+        An EmptyQueueException should be raised when one tries to peek at the first node
+        from an empty queue.
+        """
+        with self.assertRaises(EmptyQueueException) as _:
+            self.queue.peek()
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=3)
