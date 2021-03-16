1045fc35bf33b03237e817ff5f0b50c20d9be7c6
Andreas Arnesson <andreas_osby@hotmail.com>
2021-03-16 13:35:09 +0100

    kmom05 gui

--- .dbwebb/inspect-src/kmom.d/kmom05/20_sort.bash
+++ .dbwebb/inspect-src/kmom.d/kmom05/20_sort.bash
@@ -7,13 +7,16 @@ read -p "Press to run main.py" answer
 python3 main.py
 
 read -p "Press to view sort.py" answer
-cat node.py
+cat sort.py
 
 read -p "Run test.py" answer
 python3 -m unittest test.py
 
 read -p "view test.py" answer
 cat test.py
+
+read -p "Press to view node.py" answer
+cat node.py
 
 read -p "Press to view unorderedlist.py" answer
 cat unorderedlist.py
