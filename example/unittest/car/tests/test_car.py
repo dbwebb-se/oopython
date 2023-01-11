860369034eaa6d0abc8808cc93967d747e857a1c
Marie Grahn <epkmagr@gmail.com>
2023-01-11 12:35:32 +0100

    Updated unittest car with setup and seed.

--- example/unittest/car/tests/test_car.py
+++ example/unittest/car/tests/test_car.py
@@ -6,6 +6,11 @@ from src.car import Car
 
 class TestCar(unittest.TestCase):
     """ Submodule for unittests, derives from unittest.TestCase """
+    
+    def setUp(self):
+        """ Setup that runs before every testcase """
+        random.seed("car")
+        
     def test_no_of_wheels_ok(self):
         """ Test if number of wheels is 4 """
         my_car = Car("Volvo", 50000) # Act
@@ -16,3 +21,10 @@ class TestCar(unittest.TestCase):
         my_car = Car("Volvo", 50000) # Act
         self.assertEqual(my_car.present_car(),
             "This car is of model Volvo and costs 50000$.")# Assert
+
+    def test_random_numbers(self):
+        """ Test random numbers from 1 to 100 """
+        print("First number: ", random.randint(1, 100))
+        print("Second number: ", random.randint(1, 100))
+        # Assert random number 1 and 2
+        
