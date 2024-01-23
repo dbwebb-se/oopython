9833b1c59436724a4b4ff24a99cf4a2e506c76e2
Andreas Arnesson <Andreas_osby@hotmail.com>
2024-01-23 09:21:52 +0100

    Sends hashed id for sentry to test

--- .dbwebb/test/scripts.d/examiner.d.bash
+++ .dbwebb/test/scripts.d/examiner.d.bash
@@ -2,6 +2,7 @@
 
 . "$COURSE_REPO_BASE/.dbwebb/test/functions.bash"
 
+
 HEADER="scripts.d/$( basename -- "$0" ) start"
 FOOTER="scripts.d/$( basename -- "$0" ) end"
 
@@ -39,9 +40,11 @@ if test -f "$COURSE_REPO_BASE/.dbwebb.sentry"; then
     fi
 fi
 
+USERHASH="$((0x$(sha1sum <<<"$ACRONYM")0))"
+
 status=0
 for path_ in $PATHS; do
-    bash -c "set -o pipefail && cd '${DIR}/..' && '${PYTHON_EXECUTER}' -m '${EXAMINER_RUNNER}' --what='${path_}' '${disable_sentry}' --sentry_url='${SENTRY_URL}' --sentry_release='${SENTRY_RELEASE}' --sentry_sample_rate='${SENTRY_SAMPLE_RATE}' ${ARGUMENTS} 2>&1 | tee -a "$LOG" "
+    bash -c "set -o pipefail && cd '${DIR}/..' && '${PYTHON_EXECUTER}' -m '${EXAMINER_RUNNER}' --what='${path_}' '${disable_sentry}' --sentry_url='${SENTRY_URL}' --sentry_release='${SENTRY_RELEASE}' --sentry_sample_rate='${SENTRY_SAMPLE_RATE}' --sentry_user=${USERHASH} ${ARGUMENTS} 2>&1 | tee -a "$LOG" "
     status=$(($? + $status))
 done;
 
