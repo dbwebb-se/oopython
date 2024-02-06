a363210399a55f8469f27084d7f374581b424901
Marie Grahn <52319033+epkmagr@users.noreply.github.com>
2024-02-06 09:34:19 +0100

    Added better asserts in kmom02_account

--- example/unittest/kmom02_account/tests/test_account.py
+++ example/unittest/kmom02_account/tests/test_account.py
@@ -16,12 +16,18 @@ class TestAccount(unittest.TestCase):
         """ Test owner """
         my_account = Account(10000, "studiemedel", "Marie") # Arrange
         test_owner = my_account.owner # Act
+        self.assertNotEqual(test_owner, "", "Owner should not be empty string") # Assert
         self.assertEqual(test_owner, "Marie", "Owner should be Marie") # Assert
 
     def test_create_account_ok(self):
         """ Test that instance is Account """
         my_account = Account(10000, "studiemedel", "Marie") # Arrange
-        self.assertIsInstance(my_account, Account, "Instance should be Account") # Assert
+        # Assert
+        self.assertEqual(my_account.name, "studiemedel", "Name should be studiemedel")
+        self.assertTrue(my_account.number >= 1000000, "Account number should be larger or equal to 1000000")
+        self.assertGreaterEqual(my_account.number, 1000000, "Account number should be larger or equal to 1000000")
+        self.assertTrue(my_account.number <= 20000000, "Account number should be lesser or equal to 20000000")
+        self.assertLessEqual(my_account.number, 20000000, "Account number should be lesser or equal to 20000000")
 
     def test_create_account_number_ok(self):
         """ Test that account number is set """
@@ -33,7 +39,8 @@ class TestAccount(unittest.TestCase):
         """ Test that second account number not is the same as first """
         my_account = Account(10000, "studiemedel", "Marie") # Arrange
         my_charity = Account(500, "välgörenhet", "Marie") # Arrange
-        self.assertNotEqual(my_charity.number, 7133616, "Number should not be 7133616") # Assert
+        self.assertNotEqual(my_charity.number, 7133616, "Number should not be 7133616") # Assert - not good enough
+        self.assertEqual(my_charity.number, 5911104, "Number should be 7133616") # Assert
 
     def test_account_balance_ok(self):
         """ Test that account balance is set """
